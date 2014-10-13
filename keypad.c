3603f362ac671a1e5175c4d7e85fc89f9f14d5d5
vacuumtube <jhgentry@email.arizona.edu>
2014-10-13 14:49:28 -0700

    New Keypad function. needs to be tested

--- keypad.c
+++ keypad.c
@@ -26,37 +26,223 @@
 #define TRIS_COL3 TRISAbits.TRISA4
 
 
+#define HIGH 1
+#define LOW 0
+
+
 //******************************************************************************************** //
+
+volatile int col = 0;
+
 void KeypadInitialize() {
-	
-	// TODO: Configure IOs and Change Notificaiton interrupt for keypad scanning. This 
-	// configuration should ensure that if any key is pressed, a change notification interrupt 
+
+	// TODO: Configure IOs and Change Notificaiton interrupt for keypad scanning. This
+	// configuration should ensure that if any key is pressed, a change notification interrupt
 	// will be generated.
+
+    TRIS_ROW1 = 0; TRIS_COL1 = 1;
+    TRIS_ROW2 = 0; TRIS_COL2 = 1;
+    TRIS_ROW3 = 0; TRIS_COL3 = 1;
+    TRIS_ROW4 = 0;
+
+    AD1CON3bits.ADCS0 = 1;
+    AD1CON3bits.ADCS1 = 1;
+
+
+    //set cn2,cn3 and cn0 pullup resister off and the interupt on
+    CNPU1 = 0x000D;
+
+    CNEN1 = 0x000D;
+
+//    IFS1bits.CNIF = 0;  // 23
+//    IEC1bits.CNIE = 1;
+
+    ROW1 = LOW;
+    ROW2 = LOW;
+    ROW3 = LOW;
+    ROW4 = LOW;
+
 }
 
 // ******************************************************************************************* //
 
 char KeypadScan() {
-	char key = -1;
-	
-	
-	// TODO: Implement the keypad scanning procedure to detect if exactly one button of the 
+
+
+    // TODO: Implement the keypad scanning procedure to detect if exactly one button of the
 	// keypad is pressed. The function should return:
 	//
 	//      -1         : Return -1 if no keys are pressed.
-	//      '0' - '9'  : Return the ASCII character '0' to '9' if one of the 
+	//      '0' - '9'  : Return the ASCII character '0' to '9' if one of the
 	//                   numeric (0 - 9) keys are pressed.
-	//      '#'        : Return the ASCII character '#' if the # key is pressed. 
-	//      '*'        : Return the ASCII character '*' if the * key is pressed. 
+	//      '#'        : Return the ASCII character '#' if the # key is pressed.
+	//      '*'        : Return the ASCII character '*' if the * key is pressed.
 	//       -1        : Return -1 if more than one key is pressed simultaneously.
-	// Notes: 
-	//        1. Only valid inputs should be allowed by the user such that all invalid inputs 
+	// Notes:
+	//        1. Only valid inputs should be allowed by the user such that all invalid inputs
 	//           are ignored until a valid input is detected.
 	//        2. The user must release all keys of the keypad before the following key press
-	//           is processed. This is to prevent invalid keypress from being processed if the 
+	//           is processed. This is to prevent invalid keypress from being processed if the
 	//           users presses multiple keys simultaneously.
-	//
-	return key;
+
+	char key = -1;
+
+        int row = 1;
+        int count = 0;
+
+
+        for(row = 1; row < 5; row++){
+            if(row == 1){
+                ROW1 = LOW;
+                ROW2 = HIGH;
+                ROW3 = HIGH;
+                ROW4 = HIGH;
+
+                if(COL1 == LOW && COL2 == HIGH && COL3 == HIGH){ key = '1'; count++;}
+                if(COL1 == HIGH && COL2 == LOW && COL3 == HIGH){ key = '2'; count++;}
+                if(COL1 == HIGH && COL2 == HIGH && COL3 == LOW){ key = '3'; count++;}
+
+            }
+            else if(row == 2){
+                ROW1 = HIGH;
+                ROW2 = LOW;
+                ROW3 = HIGH;
+                ROW4 = HIGH;
+
+                if(COL1 == LOW && COL2 == HIGH && COL3 == HIGH){ key = '4'; count++;}
+                if(COL1 == HIGH && COL2 == LOW && COL3 == HIGH){ key = '5'; count++;}
+                if(COL1 == HIGH && COL2 == HIGH && COL3 == LOW){ key = '6'; count++;}
+
+
+            }
+            else if(row == 3){
+                ROW1 = HIGH;
+                ROW2 = LOW;
+                ROW3 = HIGH;
+                ROW4 = HIGH;
+
+                if(COL1 == LOW && COL2 == HIGH && COL3 == HIGH){ key = '7'; count++;}
+                if(COL1 == HIGH && COL2 == LOW && COL3 == HIGH){ key = '8'; count++;}
+                if(COL1 == HIGH && COL2 == HIGH && COL3 == LOW){ key = '9'; count++;}
+
+            }
+            else if(row == 4){
+                ROW1 = HIGH;
+                ROW2 = LOW;
+                ROW3 = HIGH;
+                ROW4 = HIGH;
+
+                if(COL1 == LOW && COL2 == HIGH && COL3 == HIGH){ key = '*'; count++;}
+                if(COL1 == HIGH && COL2 == LOW && COL3 == HIGH){ key = '0'; count++;}
+                if(COL1 == HIGH && COL2 == HIGH && COL3 == LOW){ key = '#'; count++;}
+
+            }
+
+
+        }
+
+    ROW1 = LOW;
+    ROW2 = LOW;
+    ROW3 = LOW;
+    ROW4 = LOW;
+
+    if(count == 1){
+         return key;
+    }
+    else
+         return -1;
+
+//        while (row <= 5){
+//            if (row == 5){
+//                ROW1 = 0;
+//                ROW2 = 0;
+//                ROW3 = 0;
+//                ROW4 = 0;
+//                if (COL1==0&&COL2==0){
+//                    col = 0;
+//                } else if (COL1==0&&COL3==0){
+//                    col = 0;
+//                } else if (COL2==0&&COL3==0){
+//                    col = 0;
+//                }
+//                while( COL1==0&&COL2==0&&COL3==0);
+//            } else if (row == 1){
+//                ROW2 = 1;
+//                ROW3 = 1;
+//                ROW4 = 1;
+//                ROW1 = 0;
+//                 if (!COL1&&COL2&&COL3){
+//                    col = 1;
+//                } else if (COL1&&!COL2&&COL3) {
+//                    col = 2;
+//                } else if (COL1&&COL2&&!COL3) {
+//                    col = 3;
+//                }
+//            } else if (row == 2){
+//                ROW1 = 1;
+//                ROW3 = 1;
+//                ROW4 = 1;
+//                ROW2 = 0;
+//                if (!COL1&&COL2&&COL3){
+//                    col = 1;
+//                } else if (COL1&&!COL2&&COL3) {
+//                    col = 2;
+//                } else if (COL1&&COL2&&!COL3) {
+//                    col = 3;
+//                }
+//            } else if (row == 3){
+//                ROW1 = 1;
+//                ROW2 = 1;
+//                ROW4 = 1;
+//                ROW3 = 0;
+//                 if (!COL1&&COL2&&COL3){
+//                    col = 1;
+//                } else if (COL1&&!COL2&&COL3) {
+//                    col = 2;
+//                } else if (COL1&&COL2&&!COL3) {
+//                    col = 3;
+//                }
+//            } else if (row == 4) {
+//                ROW1 = 1;
+//                ROW2 = 1;
+//                ROW3 = 1;
+//                ROW4 = 0;
+//                if (!COL1&&COL2&&COL3){
+//                    col = 1;
+//                } else if (COL1&&!COL2&&COL3) {
+//                    col = 2;
+//                } else if (COL1&&COL2&&!COL3) {
+//                    col = 3;
+//                }
+//            }
+//            row++;
+//
+//
+//        ROW1 = 0;
+//        ROW2 = 0;
+//        ROW3 = 0;
+//        ROW4 = 0;
+//
+//        if (col != 0){
+//            if (row = 4){
+//                if (col = 1){
+//                    key = '*';
+//                }else if (col == 2){
+//                    key = '0';
+//                } else if (col == 3){
+//                    key = '#';
+//                } else {
+//                    return -1;
+//                }
+//            } else {
+//                key = '1'+col-1+(row*3);
+//            }
+//        }
+//        }
+
+
+
+//       	return key;
 }
 
 // ******************************************************************************************* //
